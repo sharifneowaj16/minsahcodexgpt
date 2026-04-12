53dc26c5cbf147b4201a6027d56048cc3b6b5bc5
sharifneowaj16 <sharifneowaja16@gmail.com>
2026-04-12 14:14:02 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,9 +21,10 @@ const nextConfig: NextConfig = {
   },
 
   env: {
-  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL ?? "https://minsahbeauty.cloud",
   NEXT_PUBLIC_REALTIME_WS_URL: "wss://realtime.minsahbeauty.cloud/ws",
   NEXT_PUBLIC_WS_AUTH_SECRET: "438e261811e8de9de98d1d1d4b87d68d315fa1494166226f",
+  NEXT_PUBLIC_MINIO_PUBLIC_URL: process.env.NEXT_PUBLIC_MINIO_PUBLIC_URL ?? "https://minio.minsahbeauty.cloud",
   },
 
   experimental: {
